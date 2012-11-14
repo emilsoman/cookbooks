3202979261a51b213f80a0945ff172395ebec214
Emil S <emil.soman@gmail.com>
2012-11-14 18:51:09 +0530

    Removed triggered deploy

--- cookbooks/triggered_deploy/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer       "Emil Soman"
-maintainer_email "emil.soman@gmail.com"
-license          "MIT"
-description      "Installs/Configures triggered_deploy"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
-depends          "application"
