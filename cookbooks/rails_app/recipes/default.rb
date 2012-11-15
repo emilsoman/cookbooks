bf96e6fa7473060cc8bcb88e81c61126457e9330
Emil S <emil.soman@gmail.com>
2012-11-15 15:01:38 +0530

    1)Running Unicorn as Root 2)Nodes should look at cloud_environment_id to determine related nodes

--- cookbooks/rails_app/recipes/default.rb
+++ cookbooks/rails_app/recipes/default.rb
@@ -7,9 +7,13 @@
 # License - MIT
 #
 
-puts "_________________"
-puts data_bag('nodes').inspect
-puts "_________________"
+puts "Available nodes in data bag = #{data_bag('nodes').inspect}"
+if node['cloud_environment_id'].nil?
+  environment_id = data_bag_item('nodes', Chef::Config[:node_name])['environment_id']
+  node.set['cloud_environment_id'] = environment_id
+end
+puts "Cloud Environment ID of node #{Chef::Config[:node_name]} = #{node.cloud_environment_id.inspect}"
+
 
 =begin
 Chef::Log.info("Printing : #{data_bag('nodes').inspect}")
@@ -19,16 +23,24 @@ puts "data bags for projects = #{data_bag('projects')}"
 project_name = data_bag_item('projects', project_id)['name']
 puts "project name = #{project_name}"
 =end
+project_name = "test_app"
+
+user "deploy" do
+  comment "Deployment user added by VisualCloud"
+end
+
+package "libsqlite3-dev"
 
 application "rails_app" do
-  owner "deploy"
+  owner 'ubuntu'
+  group 'admin'
   path "/deploy/#{project_name}"
   repository "git@github.com:emilsoman/rails_test_app.git"
   revision "HEAD"
-  environment_name "production"
+  environment_name "development"
   rails do
+    gems ["bundler"]
     database_master_role "db"
-    precompile_assets true
     database do
       database "#{project_name}_production"
       username "root"
